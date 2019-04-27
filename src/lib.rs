1965f5f40ebbb6a16c557695147aeacdfeeb84ec
David Barsky <dbarsky@amazon.com>
2019-04-27 13:43:41 -0400

    removed hanging tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,37 +1,28 @@
 #![feature(async_await, await_macro)]
 
-use http::{Method, Request, Response, StatusCode, Uri};
+use http::Request;
 use hyper::client::HttpConnector;
-use serde::{Deserialize, Serialize};
 use std::time::Duration;
-use tokio::{
-    await,
-    prelude::{Async, Poll},
-};
+use tokio::await;
 use tower::{
     builder::ServiceBuilder, limit::RateLimit, load_shed::LoadShed, timeout::Timeout,
     util::ServiceExt, Service,
 };
-use tower_test::{assert_request_eq, mock};
 
 pub use tower_hyper::{client::Client, Body};
 
 type Error = Box<dyn std::error::Error + Send + Sync>;
 
-#[derive(Serialize, Deserialize, Debug)]
-struct Origin {
-    origin: String,
-}
-
-pub struct Svc<T>
+pub struct Svc<T, R>
 where
-    T: Service<Request<Body>>,
+    T: Service<R>,
     T::Error: Into<Error>,
 {
     inner: Timeout<RateLimit<LoadShed<T>>>,
+    _phan: std::marker::PhantomData<R>,
 }
 
-impl<T, R> Svc<T>
+impl<T, R> Svc<T, R>
 where
     T: Service<R>,
     T::Error: Into<Error>,
@@ -42,7 +33,10 @@ where
             .rate_limit(100, Duration::from_millis(100))
             .load_shed()
             .service(svc);
-        Self { inner }
+        Self {
+            inner,
+            _phan: std::marker::PhantomData,
+        }
     }
 
     pub async fn call(&mut self, req: R) -> Result<T::Response, Error> {
@@ -52,35 +46,8 @@ where
     }
 }
 
-impl Default for Svc<Client<HttpConnector, Body>> {
+impl Default for Svc<Client<HttpConnector, Body>, Request<Body>> {
     fn default() -> Self {
         Svc::new(Client::new())
     }
-}
-
-type Mock = mock::Mock<Request<Body>, Response<Body>>;
-type Handle = mock::Handle<Request<Body>, Response<Body>>;
-
-fn new_mock() -> (Svc<Mock>, Handle) {
-    let (svc, handle) = mock::pair();
-    (Svc::new(svc), handle)
-}
-
-#[test]
-fn hello() -> Result<(), Error> {
-    let (mut svc, mut handle) = new_mock();
-
-    let uri: Uri = "http://httpbin.org/ip".parse()?;
-    let req = Request::builder()
-        .method(Method::GET)
-        .uri(uri)
-        .body(Body::from(hyper::Body::empty()))?;
-
-    let mut response = svc.call(req);
-
-    let send_response = Response::builder()
-        .status(StatusCode::OK)
-        .body(Body::from(hyper::Body::empty()))?;
-
-    Ok(())
 }
